6d0ea03b9df375f200aaade38dbaf67e41588fdc
Roma <nikitinroma2605@gmail.com>
2023-07-13 18:06:58 +0300

    Validation added

--- src/main/java/com/rom4ik/firstrestapp/model/Student.java
+++ src/main/java/com/rom4ik/firstrestapp/model/Student.java
@@ -1,5 +1,7 @@
 package com.rom4ik.firstrestapp.model;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,9 +12,24 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Student {
     private int id;
+
+    @NotEmpty(message = "Group should not be empty!")
+    @NotNull(message = "Group should not be null!")
     private String group;
+
+    @NotEmpty(message = "First name should not be empty!")
+    @NotNull(message = "First name should not be null!")
     private String firstName;
+
+    @NotEmpty(message = "Last name should not be empty!")
+    @NotNull(message = "Last name should not be null!")
     private String lastName;
+
+    @NotEmpty(message = "Group should not be empty!")
+    @NotNull(message = "Group should not be null!")
     private String birthday;
+
+    @NotEmpty(message = "Gender should not be empty!")
+    @NotNull(message = "Gender should not be null!")
     private String gender;
 }
